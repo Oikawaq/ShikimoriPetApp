224f33c46f6e405601244aa0831225c657eb3598
oikawaq <ivanilkiv8@mail.ru>
2026-01-04 20:05:53 +0800

    Initial Commit

---
+++ ShikimoriPetAppTests/ShikimoriPetAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShikimoriPetAppTests.swift
+//  ShikimoriPetAppTests
+//
+//  Created by Иван Илькив on 1/4/26.
+//
+
+import Testing
+@testable import ShikimoriPetApp
+
+struct ShikimoriPetAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
